606e063f0cd6308f28596e0c38ace70ddbb3225b
Alain Gyger <Alain@theGygers.com>
2023-01-04 16:17:29 -0800

    Add line split logic.

--- Scratch.py
+++ Scratch.py
@@ -1,7 +1,27 @@
 import subprocess
 
-result = subprocess.run(['nmap', '127.0.0.1', '--open'], stdout=subprocess.PIPE)
+ip_to_scan = '8.8.8.8'
+result = subprocess.run(['nmap', ip_to_scan, '-p-', '-T5'], stdout=subprocess.PIPE)
 nmap_scan_output_raw = result.stdout.decode('utf-8')
 
-for line in nmap_scan_output_raw.split("\n"):
+nmap_result_list = []
+nmap_result_dict = {"HostIP": ip_to_scan, "HostName": "", "HostState": "", "HostLatencySeconds": "", "ScanTimeSeconds": ""}
+
+for line_with_multiple_spaces in nmap_scan_output_raw.split("\n"):  # Replace multiple spaces with one
+    line = ' '.join(line_with_multiple_spaces.split())
     print(line)
+    if "Nmap scan report for" in line:
+        nmap_result_dict["HostIP"] = line.split(' ')[5][1:-1]
+        nmap_result_dict["HostName"] = line.split(' ')[4]
+    if "Host is " in line:
+        nmap_result_dict["HostState"] = line.split(' ')[2]
+        nmap_result_dict["HostLatencySeconds"] = line.split(' ')[3][1:-1]
+    if "Note: Host seems down." in line:
+        nmap_result_dict["HostState"] = line.split(' ')[3][:-1]
+    if "/tcp" in line or "/udp" in line:
+        nmap_result_dict.update({line.split(' ')[0] + '/' + line.split(' ')[1]: line.split(' ')[2]})
+    if " scanned in " in line:
+        nmap_result_dict["ScanTimeSeconds"] = line.split(' ')[10]
+
+
+print(nmap_result_dict)
